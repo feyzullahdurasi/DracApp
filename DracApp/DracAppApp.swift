34e04db6523b5d78733520619a0fe74b246f886f
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-08-02 16:31:12 +0300

    Initial Commit

---
+++ DracApp/DracAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DracAppApp.swift
+//  DracApp
+//
+//  Created by Feyzullah Durası on 2.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DracAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
