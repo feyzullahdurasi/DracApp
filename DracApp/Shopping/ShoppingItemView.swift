edb805e96d91a153367aeefede11353c71866ae0
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-08-11 14:07:08 +0300

    shopping page has been created.

---
+++ DracApp/Shopping/ShoppingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingItemView.swift
+//  DracApp
+//
+//  Created by Feyzullah Durası on 11.08.2024.
+//
+
+import SwiftUI
+
+struct ShoppingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingItemView()
+}
---
+++ DracApp/Shopping/ShoppingView.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingView.swift
+//  DracApp
+//
+//  Created by Feyzullah Durası on 11.08.2024.
+//
+
+import SwiftUI
+
+struct ShoppingView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ShoppingView()
+}
